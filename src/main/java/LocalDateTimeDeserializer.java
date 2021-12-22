ce807c63641fdf5c93f3a8d68a90116b7b2bc9ca
danisz118 <daniz118@gmail.com>
2021-12-22 18:39:22 +0200

    hw 12

---
+++ src/main/java/LocalDateTimeDeserializer.java
@@ -0,0 +1,16 @@
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
+    public LocalDateTimeDeserializer(){
+        super(LocalDateTime.class);
+    }
+    @Override
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        return LocalDateTime.parse(p.readValueAs(String.class));
+    }
+}
